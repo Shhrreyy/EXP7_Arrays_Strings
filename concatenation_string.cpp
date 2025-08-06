3c59a37aa36a6950798e4a24e5c3be6b0a00b0a9
Shreyy <shriyanshyadav2006@gmail.com>
2025-08-06 22:10:23 +0530

    Create concatenation_string.cpp

---
+++ concatenation_string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string firstName = "Shrey";
+    string lastName = "Yadav";
+    string fullName = firstName + " " + lastName;
+
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
